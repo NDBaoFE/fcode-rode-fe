c03d32fbc81a15550618495163c96407c44cd1b3
markhipz <cnhh85@gmail.com>
2022-08-08 02:31:21 +0700

    add login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        modal: '#0a0409',
+        box: '#EFEEF1',
+      },
+      backgroundImage: {
+        home: 'url("./assets/img/bg.png")',
+      },
+    },
   },
   plugins: [],
 }
