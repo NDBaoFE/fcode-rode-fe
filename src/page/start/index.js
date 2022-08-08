68d974d748165d07d6852225415f7cc370bb85c9
markhipz <cnhh85@gmail.com>
2022-08-08 17:52:41 +0700

    add enter event for input and re-align input box

--- src/page/start/index.js
+++ src/page/start/index.js
@@ -15,20 +15,26 @@ const Start = () => {
     return (
       <div className="m-auto w-[28rem] h-[20rem] bg-modal rounded-lg drop-shadow-md p-6">
         <div className="flex h-full">
-          <div className="mx-auto py-6 w-full">
+          <div className="mx-auto py-12 w-full">
             <div className="mx-auto w-full text-center text-white item-center">
               <h2 className="text-xl text">
                 Enter code to start <span className="font-black">Battle.</span>
               </h2>
-              <div className="border border-[#C4C4C4] border-b-0 w-1/4 mx-auto my-7"></div>
+              <div className="border border-[#C4C4C4] border-b-0 w-1/4 mx-auto m-2 mb-11"></div>
               <input
                 type="text"
                 value={code}
                 onChange={(event) => handleInputChange(event.target.value)}
                 className="bg-[#ffffff] text-[#41374b] text-sm text-center font-semibold py-2 px-4 rounded-lg focus:outline-none"
-                placeholder="Enter code to here"
+                placeholder="Code given by F-Code"
                 autoFocus
+                onKeyDown={(event) => {
+                  if (event.key === 'Enter') {
+                    handleSubmit()
+                  }
+                }}
               />
+
               <div
                 onClick={handleSubmit}
                 className="my-6 p-2 bg-[#1b171f] text-sm text-[#54585C] w-1/3 mx-auto rounded-lg cursor-pointer hover:text-[#f7f7f7] hover:bg-[#231e29] duration-300"
